c11206fdf9788c728cfab667e0d630651f1725c2
Kevin Chen <kevin.chen@kkday.com>
2022-01-04 18:10:04 +0800

    feat: 關閉複雜的NER判斷設定

--- src/main/java/com/hankcs/cfg/Configuration.java
+++ src/main/java/com/hankcs/cfg/Configuration.java
@@ -57,18 +57,18 @@ public class Configuration {
         this.enableIndexMode = settings.get("enable_index_mode", "false").equals("true");
         this.enableNumberQuantifierRecognize = settings.get("enable_number_quantifier_recognize", "false").equals("true");
         this.enableCustomDictionary = settings.get("enable_custom_dictionary", "true").equals("true");
-        this.enableTranslatedNameRecognize = settings.get("enable_translated_name_recognize", "true").equals("true");
+        this.enableTranslatedNameRecognize = settings.get("enable_translated_name_recognize", "false").equals("true");
         this.enableJapaneseNameRecognize = settings.get("enable_japanese_name_recognize", "false").equals("true");
         this.enableOrganizationRecognize = settings.get("enable_organization_recognize", "false").equals("true");
         this.enablePlaceRecognize = settings.get("enable_place_recognize", "false").equals("true");
-        this.enableNameRecognize = settings.get("enable_name_recognize", "true").equals("true");
+        this.enableNameRecognize = settings.get("enable_name_recognize", "false").equals("true");
         this.enableTraditionalChineseMode = settings.get("enable_traditional_chinese_mode", "false").equals("true");
         this.enableStopDictionary = settings.get("enable_stop_dictionary", "false").equals("true");
         this.enablePartOfSpeechTagging = settings.get("enable_part_of_speech_tagging", "false").equals("true");
-        this.enableRemoteDict = settings.get("enable_remote_dict", "true").equals("true");
+        this.enableRemoteDict = settings.get("enable_remote_dict", "false").equals("true");
         this.enableNormalization = settings.get("enable_normalization", "false").equals("true");
         this.enableOffset = settings.get("enable_offset", "true").equals("true");
-        this.enableCustomConfig = settings.get("enable_custom_config", "false").equals("true");
+        this.enableCustomConfig = settings.get("enable_custom_config", "true").equals("true");
         Dictionary.initial(this);
     }
 
